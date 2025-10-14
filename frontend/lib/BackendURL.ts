9bf867d8132e04c848a816b55f17fde15b24cb10
Frasier <fsundra@gmail.com>
2025-10-14 23:22:53 +0800

    testing

--- frontend/lib/BackendURL.ts
+++ frontend/lib/BackendURL.ts
@@ -1,3 +1,6 @@
 export const getBackendUrl = () => {
-    return process.env.BACKEND_INTERNAL_URL || process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5141";
+    if (typeof window === "undefined") {
+        return process.env.BACKEND_INTERNAL_URL || "http://backend:8080";
+    }
+    return process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5141";
 };
